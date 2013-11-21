1ae2cdd16d7888e4b703a43079ef2c9ae06220dd
ipostanogov <ipostanogov@outlook.com>
2013-11-21 23:13:39 +0600

    Fixed gate.xml error Added local file loading example

--- src/main/scala/InformationRetrievalApp.scala
+++ src/main/scala/InformationRetrievalApp.scala
@@ -1,5 +1,7 @@
 /*
- *  InformationRetrievalApp.java
+ *  Example of loading and querying local & non-local documents *
+ *  Igor Postanogov, 21/Nov/2013                                *
+ *  Based on: http://gate.ac.uk/wiki/code-repository/src/sheffield/examples/InformationRetrievalApp.java
  *
  *  Copyright (c) 1998-2003, The University of Sheffield.
  *
@@ -10,7 +12,6 @@
  *
  *  Marin Dimitrov, 27/Jan/2003
  *
- *  $Id: InformationRetrievalApp.java,v 1.4 2004/12/14 14:36:24 niraj Exp $
  */
 
 import java.io.File
@@ -26,28 +27,30 @@ import org.apache.commons.io.FileUtils
 import org.apache.commons.io.FilenameUtils.concat
 import org.apache.log4j.{Level, Logger}
 
-object InformationRetrievalApp {
+object InformationRetrievalApp extends App {
   val appFolderPath = concat(System.getProperty("user.home"), ".textyour")
   val indexLocationPath: String = concat(appFolderPath, "index")
   val pluginsFolderPath = concat(appFolderPath, "plugins")
   val serialDataStoreFilePath: String = concat(appFolderPath, "datastore")
+  val filesToLoadFolderPath: String = concat(appFolderPath, "user files")
 
   def initFolders() {
-    scala.collection.Set(appFolderPath, indexLocationPath, pluginsFolderPath).foreach({
+    Set(appFolderPath, indexLocationPath, pluginsFolderPath, filesToLoadFolderPath) foreach {
       new File(_) {
         if (!exists() && !mkdirs())
           throw new Exception
       }
-    })
+    }
     new File(serialDataStoreFilePath) {
       if (exists())
         FileUtils.deleteDirectory(this)
     }
   }
 
-  def main(args: Array[String]) {
+  run()
+
+  def run() {
     Logger.getRootLogger.setLevel(Level.OFF)
-    val irApp = new InformationRetrievalApp
     try {
       initFolders()
       System.setProperty("gate.plugins.home", pluginsFolderPath)
@@ -63,9 +66,10 @@ object InformationRetrievalApp {
       }
     }
     try {
+      // <boring magic> Always copy-paste it into your program (maybe):
       val sds: DataStore = Factory.createDataStore("gate.persist.SerialDataStore", "file:///" + serialDataStoreFilePath)
       sds.open()
-      val corpus: Corpus = irApp.createTestCorpus()
+      val corpus: Corpus = createTestCorpus()
       val serialCorpus: Corpus = sds.adopt(corpus, null).asInstanceOf[Corpus]
       sds.sync(serialCorpus)
       val indexedCorpus: IndexedCorpus = serialCorpus.asInstanceOf[IndexedCorpus]
@@ -80,6 +84,7 @@ object InformationRetrievalApp {
       indexedCorpus.getIndexManager.optimizeIndex()
       val search: Search = new LuceneSearch
       search.setCorpus(indexedCorpus)
+      // </boring magic>
       val query: String = "+content:\"until there's a cure\" +author:foundation"
       val res: QueryResultList = search.search(query)
       val it: util.Iterator[_] = res.getQueryResults
@@ -104,6 +109,18 @@ object InformationRetrievalApp {
         Out.prln("Query2: author = " + resultDoc.getFeatures.get("author"))
         Out.prln("------------")
       }
+      val query3: String = "+content:\"Copernic Summarizer\""
+      val res3: QueryResultList = search.search(query3)
+      val it3: util.Iterator[_] = res3.getQueryResults
+      while (it3.hasNext) {
+        val qr: QueryResult = it3.next.asInstanceOf[QueryResult]
+        val score: Float = qr.getScore
+        val resultDoc: Document = sds.getLr("gate.corpora.DocumentImpl", qr.getDocumentID).asInstanceOf[Document]
+        Out.prln("Query3: DOC_NAME=" + resultDoc.getName)
+        Out.prln("Query3: score = " + score)
+        Out.prln("Query3: author = " + resultDoc.getFeatures.get("author"))
+        Out.prln("------------")
+      }
       Out.prln("done...")
     }
     catch {
@@ -112,21 +129,27 @@ object InformationRetrievalApp {
       }
     }
   }
-}
 
-class InformationRetrievalApp {
   def createTestCorpus() = {
+    // Load from url example
     val doc1: Document = Factory.newDocument(new URL("http://www.wish.org/"))
     doc1.getFeatures.put("author", "Make-A-Wish Foundation")
     doc1.setName("Make-A-Wish document")
-    val doc2: Document = Factory.newDocument(new URL("http://www.until.org"))
+    // And another one
+    val doc2: Document = Factory.newDocument(new URL("http://web.archive.org/web/20021128120434/http://cob868.dn.net/"))
     doc2.getFeatures.put("author", "Until There's A Cure Foundation")
     doc2.setName("until.org document")
-    assert(doc1 != null && doc2 != null)
+    FileUtils.copyFileToDirectory(new File(getClass.getResource("/v25i05.pdf").toURI), new File(filesToLoadFolderPath))
+    // Load from local file example
+    val doc3: Document = Factory.newDocument(new URL("file:///" + concat(filesToLoadFolderPath, "v25i05.pdf")))
+    doc3.getFeatures.put("author", "Ingo Feinerer, Kurt Hornik, David Meyer")
+    doc3.setName("Journal of Statistical Software")
+    assert(doc1 != null && doc2 != null && doc3 != null)
     val result: Corpus = Factory.newCorpus("test corpus")
     assert(result != null)
     result.add(doc1)
     result.add(doc2)
+    result.add(doc3)
     result
   }
 }
